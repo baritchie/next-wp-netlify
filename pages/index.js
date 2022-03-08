e0306876e5b43c4031c93cc0667c655775c6f10b
Benjamin Rojas-Ritchie <ben@benarr.com>
2022-03-08 12:06:03 -0800

    fix header component path

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import Header from "../components/header"
+import Header from "components/header"
 import { getNavItems } from "../lib/api"
 
 export default function Home({ navItems }) {
